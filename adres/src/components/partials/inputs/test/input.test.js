b10acfd83cea1f3c9c63e502facc49e3d378e0de
feres1994 <feresfatnassi@gmail.com>
2022-06-22 18:08:42 +0100

    test how input change its value

--- adres/src/components/partials/inputs/test/input.test.js
+++ adres/src/components/partials/inputs/test/input.test.js
@@ -1,20 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Input  from "../input"
 
+const mockSetInput = jest.fn()
 
 describe('Input', () => {
 it('should render same text passed into a label prop', () => {
     render(<Input
-        label="Application ID"
-   
-      />)
+        label="Application ID" />)
 
     const labelElement= screen.getByText(/Application ID/i)
     expect(labelElement).toBeInTheDocument()
  
 
+})
+
+it('should render an input element', () => {
+  render(<Input
+      label="Application ID"
+      placeholder="application id"/>)
+
+  const inputElement= screen.getByPlaceholderText(/application id/i)
+  expect(inputElement).toBeInTheDocument()
+})
+
+
+it('should be able to fill an input', () => {
+  render(<Input
+      label="Application ID"
+      placeholder="application id"
+      getInputValue={mockSetInput}
+      />)
+
+  const inputElement= screen.getByPlaceholderText(/application id/i)
+  fireEvent.change(inputElement, {target: {value: "app id"}})
+  expect(inputElement.value).toBe("app id")
 })
 
 
